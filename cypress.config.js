df98d9f4e7c89e5064e44275adae76d16e736ffd
Anissa <anissa.adv@gmail.com>
2026-03-10 20:20:54 -0300

    configuracoes github actions e allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true,
+
+  env: {
+    allure: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@shelex/cypress-allure-plugin/writer')(on, config);
+      return config;
     },
   },
 });
